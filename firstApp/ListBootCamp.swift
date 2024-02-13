13694525c4a6193121acb0b4cd5ff846fcf5a776
Abdelaziz Salah <abdelaziz132001@gmail.com>
2024-02-13 03:12:19 +0200

    1 More Hour on Swift UI
    Navigation Views, Navigation Links, add, delete, edit, move items in lists, and styling lists.

---
+++ firstApp/ListBootCamp.swift
@@ -0,0 +1,87 @@
+//
+//  ListBootCamp.swift
+//  firstApp
+//
+//  Created by zizo on 13/02/2024.
+//
+/*
+ List technically is a VStack, however it is very powerful when we want to be able add, remove or edit items in this list.
+ 
+ Edit Button exists by default, it allows you to add, delete, or move items.
+ to be able to delete, you need to use .onDelete,
+ to be able to move, you need to use .onMove
+ 
+ the styles may be different depending on the device.
+ */
+
+import SwiftUI
+
+struct ListBootCamp: View {
+    @State var fruits: [String] = ["Apples", "Bananas", "Oranges" ,"Peachs"]
+    @State var veggies: [String] = ["Tommato", "Potato", "Carrots" ,"Cucumbers"]
+    var body: some View {
+        NavigationView {
+            List {
+                Section(
+                    header: HStack {
+                        Text ("Fruit").font(.title).foregroundStyle(Color.orange)
+                        Image(systemName: "flame.fill")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.red)
+//                            .overlay(Color.red)
+                    }
+                ) {
+                    
+                    ForEach(fruits, id: \.self) { fruit in /// this is how we use the for each items, instead of accessing using the index.
+                        Text(fruit.capitalized.uppercased()).frame(maxWidth: .infinity).background(Color.green)
+                            .foregroundColor(.white).font(.title2).padding(.all, 5) /// this is where things gets cooler ;)
+                    }.onDelete(// this code, makes items inside the list dismissable.
+                        perform: delete
+                    )
+                    .onMove(perform: move) // this is a shorter way to make the code more cleaner.
+                    
+                    .listRowBackground(Color.yellow) // this is how we change the background color.
+                   
+                } // the default style is the Inset Grouped Style
+                
+                Section(
+                    header: Text ("Veggies")
+                ) {
+                    
+                    ForEach(veggies, id: \.self) { veggy in /// this is how we use the for each items, instead of accessing using the index.
+                        Text(veggy.capitalized.uppercased())
+                    }
+                    
+                }
+            }
+            .accentColor(.purple)
+//            .listStyle(PlainListStyle()) // this is applied on the list, not on the sections.
+            .navigationTitle("Grocery List")
+            .navigationBarItems(leading: EditButton(), trailing: AddButton )
+            
+            
+            
+        }
+        .accentColor(.pink) // this is how we change the color of tools, leading and trailing and things like this.
+    }
+    
+    var AddButton: some View {
+        Button("Add", action: add)
+    }
+    
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet) // indexSet, is already come with the onDelete
+    }
+    
+    func move(indices:IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    func add () {
+        fruits.append("Coconuts")
+    }
+}
+
+#Preview {
+    ListBootCamp()
+}
---
+++ firstApp/NavigationBootCamp.swift
@@ -0,0 +1,102 @@
+//
+//  NavigationBootCamp.swift
+//  firstApp
+//
+//  Created by zizo on 13/02/2024.
+//
+
+/*
+ 1. Navigation View would look different on different devices.
+ 2. Never embed a Navigation inside another navigation view.
+ 
+ Navigation View is similar to any View, but usually we use it to be able to navigate between screens.
+ Navigation link is how we set hyper link, with default name, then on click, we open another screen. 
+ */
+import SwiftUI
+
+struct NavigationBootCamp: View {
+    var body: some View {
+        NavigationView() {
+            ScrollView () {
+                NavigationLink("Go the next Screen", destination: NavigationScreen())
+                Text("This is a Navigation")
+                Text("This is a Navigation2")
+                Text("This is a Navigation3")
+            }
+            .navigationTitle("All Inbox")
+            .navigationBarTitleDisplayMode(.automatic) /// there are 3 possible values, inline, automatic, and large, usually we should leave it automatic, it is automatic by default
+            //            .navigationBarHidden(true) this is to hide the navigation bar.
+            .navigationBarItems(
+                leading: HStack {
+                    NavigationLink (
+                        destination: {
+                            ZStack() {
+                                Color.gray.ignoresSafeArea()
+                                Text("Person is working")
+                            }
+                        }, label: {Image(systemName: "person.fill")}
+                        
+                    )
+                    NavigationLink (
+                        destination: {
+                            ZStack() {
+                                Color.green.ignoresSafeArea()
+                                Text("Flame is working Too")
+                            }
+                        }, label: {Image(systemName: "flame.fill")}
+                        
+                    )
+                    
+                },
+                trailing:  NavigationLink (
+                    destination: {
+                        ZStack() {
+                            Color.green.ignoresSafeArea()
+                            Text("Gear is working Too")
+                        }
+                    }, 
+                    label: {Image(systemName: "gear").accentColor(.blue)}
+                    
+                )
+            )
+        }
+    }
+}
+
+struct NavigationScreen: View {
+    @Environment(\.presentationMode) var presentationMode /// we will use this to back to the last screen
+    
+    var body: some View {
+        ZStack() {
+            /// backgorund
+            backGround
+            /// content
+            content
+        }
+    }
+    
+    var backGround: some View {
+        Color.yellow.ignoresSafeArea()
+    }
+    
+    var content: some View {
+        VStack {
+            Text ("This is a new Screen")
+                .font(.title)
+                .bold()
+                .foregroundStyle(Color.black)
+                .navigationTitle("Yellow Screen")
+                .navigationBarHidden(true)
+            
+            Button("Back Button") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+        
+    }
+}
+
+
+#Preview {
+    NavigationBootCamp()
+}
